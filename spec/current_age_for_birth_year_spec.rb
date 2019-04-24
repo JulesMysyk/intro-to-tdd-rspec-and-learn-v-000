4a3fc9c40b32972d0c0ce55cdd30c7f43a8100e7
Julia Mysyk <juliaa.mysyk@gmail.com>
2019-04-24 03:38:46 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -8,13 +8,8 @@ describe "current_age_for_birth_year method" do
   end
 end
 
-`age_of_person = current_age_for_birth_year(1984)`
-`expect(age_of_person).to eq(19)`
 
 ```ruby
-it "should return the current year for a person born in year 0" do
-  age = current_age_for_birth_year(0)
-
-  expect(age).to eq(2003)
+def current_age_for_birth_year(birth_year)
 end
 ```
